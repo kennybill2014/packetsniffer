c5bce1d8a29ff8360d86712c0461d9d30f98f7f4
weihu <305977296@qq.com>
2016-11-03 18:44:54 +0800

    123

---
+++ packetsniffer/RecentRequestsCell.swift
@@ -0,0 +1,44 @@
+//
+//  RecentRequestsCell.swift
+//  packetsniffer
+//
+//  Created by lijinwei on 2016/11/3.
+//  Copyright © 2016年 ljw. All rights reserved.
+//
+
+import UIKit
+
+class RecentRequestsCell: UITableViewCell {
+
+    @IBOutlet var startConnectBtn: UIButton!
+    @IBOutlet var leftLabel: UILabel!
+    @IBOutlet var rightLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func setCellInfo(indexPath: NSIndexPath) {
+        switch indexPath.row {
+        case 2:
+            startConnectBtn.isHidden = false
+            leftLabel.isHidden = true
+            rightLabel.isHidden = true
+            
+        case 0,1:
+            startConnectBtn.isHidden = true
+            leftLabel.isHidden = false
+            rightLabel.isHidden = false
+
+        default:
+            break;
+        }
+    }
+}
---
+++ packetsniffer/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  packetsniffer
+//
+//  Created by lijinwei on 2016/11/2.
+//  Copyright © 2016年 ljw. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+private let kRecentRequestCellIdentifier = "recentRequests"
+private let kRecentRequestCachedIdentifier = "requestsCached"
+
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "App Name".localized()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - TableView DataSource & Delegate
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kRecentRequestCellIdentifier, for: indexPath) as! RecentRequestsCell
+        cell.setCellInfo(indexPath: indexPath as NSIndexPath)
+        return cell
+    }
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 3;
+    }
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
+        guard indexPath.row == 0 else {
+            return 60
+        }
+        return 43
+    }
+
+}
+
